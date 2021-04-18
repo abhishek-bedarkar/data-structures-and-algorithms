e9df41cba4586547916929537ef384df02f6a019
abhishek-bedarkar <abhishekbedarkar27@gmail.com>
2021-04-18 21:15:54 +0530

    Implemented three types of insertion functions in linked list: 1.Add at first 2.Add at last 3.Add after given Node

--- Linked list/linkedListImplementation.py
+++ Linked list/linkedListImplementation.py
@@ -17,6 +17,7 @@ class LinkedList:
         while self.curr:
             print('->', self.curr.data, end='')
             self.curr = self.curr.next
+        print()
 
     def generate_random_data(self, limit: int):
         # insert data to list
@@ -28,8 +29,41 @@ class LinkedList:
             self.curr.next = n
             self.curr = self.curr.next
 
+    def add_node_at_first(self, data: int):
+        # Add Node at first
+        n = Node(data)
+        n.next = self.head
+        self.head = n
+
+    def add_node_at_last(self, data: int):
+        n = Node(data)
+        self.curr = self.head
+        while self.curr.next:
+            self.curr = self.curr.next
+        self.curr.next = n
+
+    def add_node_after(self,after_node:int,data:int)->bool:
+        # Add after data after given node
+        self.curr = self.head
+        while self.curr.next:
+            if self.curr.data == after_node:
+                break
+            self.curr = self.curr.next
+        if self.curr.next is None:
+            return False
+        else:
+            n = Node(data)
+            n.next = self.curr.next
+            self.curr.next = n
+            return True
 
 if __name__ == '__main__':
     l = LinkedList()
     l.generate_random_data(10)
+    l.print_linked_list()
+    l.add_node_at_first(-1)
+    l.print_linked_list()
+    l.add_node_at_last(11)
+    l.print_linked_list()
+    l.add_node_after(9,10)
     l.print_linked_list()
